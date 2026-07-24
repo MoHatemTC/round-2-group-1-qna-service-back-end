e6a2c1036191e3fb715d3e7364f72541e2d05b58
omar5397 <132631778+omar5397@users.noreply.github.com>
2026-07-24 07:29:08 +0300

    Add AttemptController and services to StudentQuizzesModule

--- src/modules/student-quizzes/student-quizzes.module.ts
+++ src/modules/student-quizzes/student-quizzes.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { StudentQuizzesService } from './student-quizzes.service';
 import { StudentQuizzesController } from './student-quizzes.controller';
+import { AttemptController } from './attempt.controller';
+import { AttemptScoringService } from './attempt-scoring.service';
+import { ScoreEventsService } from './events/score-events.service';
 
 @Module({
-  controllers: [StudentQuizzesController],
-  providers: [StudentQuizzesService],
+  controllers: [StudentQuizzesController, AttemptController],
+  providers: [StudentQuizzesService, AttemptScoringService, ScoreEventsService],
 })
 export class StudentQuizzesModule {}
